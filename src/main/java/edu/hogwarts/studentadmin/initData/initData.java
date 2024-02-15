9d71d33144b2792d080c41e0fc37c333dc572903
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-02-15 11:39:55 +0100

    made init data right

--- src/main/java/edu/hogwarts/studentadmin/initData/initData.java
+++ src/main/java/edu/hogwarts/studentadmin/initData/initData.java
@@ -4,13 +4,16 @@ import edu.hogwarts.studentadmin.models.House;
 import edu.hogwarts.studentadmin.models.Student;
 import edu.hogwarts.studentadmin.repositories.HouseRepository;
 import edu.hogwarts.studentadmin.repositories.StudentRepository;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Controller;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class initData implements CommandLineRunner {
+@Controller
+public class initData implements ApplicationRunner {
     private final StudentRepository studentRepository;
     private final HouseRepository houseRepository;
 
@@ -19,10 +22,44 @@ public class initData implements CommandLineRunner {
         this.houseRepository = houseRepository;
     }
 
+
     @Override
-    public void run(String... args) throws Exception {
+    public void run(ApplicationArguments args) throws Exception {
+        generateHouse();
         generateStudents();
     }
+
+
+
+    public void generateHouse(){
+        var gryffindorColors = new ArrayList<String>();
+        gryffindorColors.add("Scarlet");
+        gryffindorColors.add("Gold");
+
+        var hufflepuffColors = new ArrayList<String>();
+        hufflepuffColors.add("Yellow");
+        hufflepuffColors.add("Black");
+
+        var ravenclawColors = new ArrayList<String>();
+        ravenclawColors.add("Blue");
+        ravenclawColors.add("Bronze");
+
+        var slytherinColors = new ArrayList<String>();
+        slytherinColors.add("Green");
+        slytherinColors.add("Silver");
+
+        var gryffindor = new House(1L, "Gryffindor", "Godric Gryffindor", gryffindorColors);
+        var hufflepuff = new House(2L, "Hufflepuff", "Helga Hufflepuff", hufflepuffColors);
+        var ravenclaw = new House(3L, "Ravenclaw", "Rowena Ravenclaw", ravenclawColors);
+        var slytherin = new House(4L, "Slytherin", "Salazar Slytherin", slytherinColors);
+
+
+        houseRepository.save(gryffindor);
+        houseRepository.save(hufflepuff);
+        houseRepository.save(ravenclaw);
+        houseRepository.save(slytherin);
+    }
+
 
     private void generateStudents() {
         var gryffindor = houseRepository.findById(1L);
@@ -48,6 +85,4 @@ public class initData implements CommandLineRunner {
         var students = new Student[]{harry, hermione, ron, neville, luna, draco, cedric, cho, ginny, seamus, dean, parvati};
         studentRepository.saveAll(Arrays.asList(students));
     }
-
-
 }
