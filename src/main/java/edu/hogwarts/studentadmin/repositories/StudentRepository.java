57b43d3c5d1618507b24e28b3f2ac82d62b0c9ec
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-02-20 19:55:04 +0100

    Student Repo setup

--- src/main/java/edu/hogwarts/studentadmin/repositories/StudentRepository.java
+++ src/main/java/edu/hogwarts/studentadmin/repositories/StudentRepository.java
@@ -3,5 +3,14 @@ package edu.hogwarts.studentadmin.repositories;
 import edu.hogwarts.studentadmin.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findFirstByFirstNameIgnoreCase(String firstName);
+
+    Optional<Student> findFirstByMiddleNameIgnoreCase(String middleName);
+
+    Optional<Student> findFirstByLastNameIgnoreCase(String lastName);
+
+    Optional<Student>findFirstByFirstNameIgnoreCaseAndMiddleNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String middleName, String lastName);
 }
