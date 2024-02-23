643714562553b02783df58d46b8048c84c596df5
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-02-23 12:45:18 +0100

    test

--- src/test/java/edu/hogwarts/studentadmin/StudentadminApplicationTests.java
+++ src/test/java/edu/hogwarts/studentadmin/StudentadminApplicationTests.java
@@ -3,7 +3,7 @@ package edu.hogwarts.studentadmin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StudentadminApplicationTests {
 
     //@Test
